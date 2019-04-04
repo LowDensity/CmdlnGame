44656b617601901ef2310083df0897a48b6445b7
LowDensity <>
2019-04-05 02:23:01 +0800

    updated Message.java and README.md

--- Game_Ground/src/game_ground/message/Message.java
+++ Game_Ground/src/game_ground/message/Message.java
@@ -5,14 +5,44 @@
  */
 package game_ground.message;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  *
  * @author Harry
+ * 用來讀取文件設定的Map，提供大量常數供使用
+ * 
  */
-public class Message {
-   
-    public Message(String xmlConfigurationName){
+public abstract class Message {
+   static int WellcomeMessage=0;
+   static int WellcomeMessage1=0;
+   static int WellcomeMessage2=0;
+   static int WellcomeMessage3=0;
+   private HashMap messages;
     
+    public Message(String configurationPath){
+        messages=new HashMap();
+        BufferedReader configFile;
+        FileReader fr;
+       try {
+           fr=new FileReader(configurationPath);
+           configFile=new BufferedReader(fr);
+           
+       } catch (FileNotFoundException ex) {
+           Logger.getLogger(Message.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       
+        
     }
     
 }
+
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-CmdLnGame2
+# CmdLnGame2
+
+## What is this?
+
+This is my JAVA Practicing project whith was origninally started few years ago during JAVA course as a self practice. And hope to share with anyone who came across this repository my small research result.
+Probably this can help someone during difficulty. 
+
+## The Project Content
+
+* A lobby to show wellcome message and to choose which game you want to play. Of course , all command line based. No GUI.(done)
+* 3 games which its pretty familiar for me and not too hard to build(done):
+    * Guessing Number
+    * Paper-Scissor-Stone
+    * TicToe
+* Unified Game Api Which allows extending with ease. 
+* Xml /Json based text resourcing(current job)
+* not-so-sure stuff  
+    * Plug-in structrue
+    * Content injection(through  Xml/Json)
+    * A set of standardized command-line command processing mechanic
+
+
+## Progress Log and Current status
+* 2019/3/24 (current) 
+    * 3 games have been done and working well.
+    * API sill need a lot polish but they do work.
+    * Beggining text resourcing.
+
+
+
+
+
